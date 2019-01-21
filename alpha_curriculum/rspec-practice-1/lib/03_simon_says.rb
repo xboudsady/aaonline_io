2ec434b65640b3c00f9abae79cb92f67ba2b274b
xboudsady <boudsadyx@gmail.com>
2019-01-21 13:42:00 -0800

    Completed repeat() rspec test

--- alpha_curriculum/rspec-practice-1/lib/03_simon_says.rb
+++ alpha_curriculum/rspec-practice-1/lib/03_simon_says.rb
@@ -1,3 +1,14 @@
 def echo(string)
     return string
 end
+
+
+def shout(string)
+    return string.upcase
+end
+
+
+def repeat(string, times=2)
+    words = Array.new(times, string)
+    words.join(" ")
+end
