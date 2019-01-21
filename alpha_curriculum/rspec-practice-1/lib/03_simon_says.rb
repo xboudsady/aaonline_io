0da969c40ac9040b09f1dc6490034f900e6a28f9
xboudsady <boudsadyx@gmail.com>
2019-01-21 14:40:45 -0800

    Completed titleize() rspec test

--- alpha_curriculum/rspec-practice-1/lib/03_simon_says.rb
+++ alpha_curriculum/rspec-practice-1/lib/03_simon_says.rb
@@ -12,3 +12,33 @@ def repeat(string, times=2)
     words = Array.new(times, string)
     words.join(" ")
 end
+
+
+def start_of_word(word, count)
+    return word[0...count]
+end
+
+def first_word(sentence)
+    words = sentence.split(" ")
+    return words.first
+end
+
+LITTLE_WORDS = [
+    "and",
+    "the",
+    "over"
+]
+
+def titleize(title)
+    words = title.split(" ")
+
+    titleized_words = words.map.with_index do |word, i|
+        if i != 0 && LITTLE_WORDS.include?(word)
+            word.downcase
+        else
+            word.capitalize
+        end
+    end
+
+    return titleized_words.join(" ")
+end
